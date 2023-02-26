6736ea62740debb829f7c63166a6db41fd7cb2a7
jimflowers <jdf.tools@gmail.com>
2023-02-26 09:22:55 -0500

    Fixed error in userController - backend now works

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -8,23 +8,24 @@ const protect = asyncHandler(async (req, res, next)=>{
     try {
       //get token from header
       token = req.headers.authorization.split(' ')[1]
-      console.log(`token: ${token}`)
+      
       //verify token
       const decoded = jwt.verify(token, process.env.JWT_SECRET)
-      console.log(decoded.id)
+      
       //Get user from token
       req.user = await User.findById(decoded.id).select('-password') //does not retrieve password
-console.log(`user: ${req.user}`)
+
     } catch (error) {
       console.log(error)
       res.status(401)
       throw new Error('Not authorized')
     }
-    }
+    next()
+  }
     if (!token){
       res.status(401)
       throw new Error('Not authorized')
     }
-})
+  })
 
 module.exports = {protect}
